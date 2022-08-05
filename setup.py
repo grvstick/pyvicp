eccc55bec4342a2586a5cff8222695f653183b06
Bob McNamara <7468667+bobmcnamara@users.noreply.github.com>
2022-08-05 11:08:13 -0700

    WIP

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 if __name__ == "__main__":
     setup(
-        name="example_project_bobmcnamara",
+        name="pyvicp",
         packages=["pyvicp"],
         use_scm_version=True,
         setup_requires=['setuptools_scm'],
